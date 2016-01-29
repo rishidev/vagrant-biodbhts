3758fa3709bd38e84f4466cdd7e2eeb14015c9fc
Rishi Nag <rn6@sanger.ac.uk>
2016-01-29 15:47:44 +0000

    first version

---
+++ provision.biodbhts.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+#basics
+apt-get update
+apt-get install -y zlib1g-dev
+apt-get install -y bioperl
+apt-get install -y git
+apt-get install -y build-essential
+apt-get install -y emacs23
+apt-get install -y cpanminus
+cpanm -f Module::Build
+
+#get Bio::DB::HTS installed
+git clone https://github.com/samtools/htslib.git
+cd htslib
+git checkout master
+perl -e "open my $in, '<','Makefile';
+open my $out,'>','Makefile.new';
+while (<$in>) {
+    chomp;
+    if (/^CFLAGS/ && !/-fPIC/) {
+	s/#.+//;  # get rid of comments
+	$_ .= \" -fPIC -Wno-unused -Wno-unused-result\";
+    }
+} continue {
+    print $out $_,"\n";
+}
+
+close $in;
+close $out;
+rename 'Makefile.new','Makefile' ; "
+make
+make install
+export HTSLIB_DIR=$(pwd)
+export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
+#TODO set LD_LIBRARY_PATH in .bashrc
+cd ..
+
+
+#get Bio::DB::HTS installed
+git clone https://github.com/Ensembl/Bio-HTS.git
+cd Bio-HTS
+perl Build.PL
+./Build
+./Build test
+./Build install
+
+
