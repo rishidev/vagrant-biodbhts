d8a9b067ee472092792fd3da496e2255092017db
Rishi Nag <rn6@sanger.ac.uk>
2016-03-29 10:10:56 +0100

    Added cpan packages for VEP

--- provision.basic.sh
+++ provision.basic.sh
@@ -8,7 +8,13 @@ apt-get install -y git
 apt-get install -y build-essential
 apt-get install -y emacs23
 apt-get install -y cpanminus
+
+#This is for Bio::DB::HTS
 cpanm -f Module::Build
+
+#These two are for the VEP
+cpanm -f File::Copy::Recursive
+cpanm -f Archive::Zip
 
 git config --global user.name "Rishi Nag"
 git config --global user.email "rishidev@users.noreply.github.com"
